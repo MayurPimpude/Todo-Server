432ffb8a614598606f9fd89a3f84c5d708778d3b
mayur <mayurpimpude@gmail.com>
2023-07-09 17:05:56 +0530

    new change

--- api/server.js
+++ api/server.js
@@ -1,17 +1,35 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('db.json');
+// const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
-server.use(jsonServer.bodyParser);
+// server.use(middlewares);
+// server.use(jsonServer.bodyParser);
 
-// Add custom routes or modifications here (if needed)
+// // Add custom routes or modifications here (if needed)
 
-server.use('/todos', router); // Set the base URL for JSON Server
+// server.use('/todos', router); // Set the base URL for JSON Server
 
-const port = 3001; // Choose the port number
+// const port = 3001; // Choose the port number
 
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
+// server.listen(port, () => {
+//   console.log(`JSON Server is running on port ${port}`);
+// });
+
+// module.exports = server
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+// Add this before server.use(router)
+
+server.use(router)
+server.listen(3001, () => {
+    console.log('JSON Server is running')
+})
+
+// Export the Server API
+module.exports = server
